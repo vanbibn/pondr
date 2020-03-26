a5ecae086cd4e4b1bcee56bf752f63cde3570772
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-03-26 00:31:22 -0400

    Final testing for parts of PONDR-FIT extract

--- Scripts/pondrFIT_extract.R
+++ Scripts/pondrFIT_extract.R
@@ -1,9 +1,37 @@
-# 
+# This was my first attempt at writting the script to extract data from 
+# PONDR-FIT (http://original.disprot.org/pondr-fit.php)
+
+# load packages
 library(readr)
 library(stringr)
 
+
+
+## test first with one url
+# read data from temporary url for PONDR-FIT data
+# start reading from line 2 (skip first line)
+fit1 <- read_table2(my_urls$url[1], col_names = FALSE, skip = 1)
+
+# add column names and write out to text file
+colnames(fit1) <- c("position", "residue", "score", "error")
+write.csv(fit1, file = paste0("Output/",my_urls$UniprotID[1],"_pondrFIT.csv"))
+
+#calculate the mean disorder score
+meanDis <- mean(fit1$score)
+
+## Percent disorder: count num resid score >0.5 and divid by total num residues
+## Taking the mean of a logical vector (ie. 1's and 0's) will give same result
+# total <- length(fit1$score)
+# numDis <- sum(fit1$score > 0.5)
+# percDisorder <- numDis/total
+percDisorder2 <- mean(fit1$score > 0.5)
+
+
+
+## Old stuff from first try =================================================
+
 # paste in temporary url fro PONDR-FIT data
-u <- url("http://original.disprot.org/temp/1582230290.859100.pondrfit")
+u <- url("http://original.disprot.org/temp/1585181708.562184.pondrfit")
 
 # Create a data frame start reading from line 2 (skip first line)
 fit1 <- read_table2(u, col_names = FALSE, skip = 1)
