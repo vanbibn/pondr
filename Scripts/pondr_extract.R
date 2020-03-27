9b6bddac5cef26c97155ce210d984f1e9e70129c
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-03-27 14:54:05 -0400

    Added comments describing each step.

--- Scripts/pondr_extract.R
+++ Scripts/pondr_extract.R
@@ -1,25 +1,48 @@
-library(readr)
+# Nathan W. Van Bibber
+# 3/27/20
+# 
+# This is a script to extract data from a test file collected from PONDR
+# <url link>
 
-getwd()
-list.files('Data')
 
-pondr <- readLines("Data/Q3BBV0_3.txt")
+# Load packages
+library(tidyverse)
 
-# remove disorder segment lines
+# create a vector of file names in listed directory
+files <- list.files("Data/pondr_text_Nathan/")
+files
+
+
+#### Loop through each file and do the following
+
+pondr <- readLines(paste0("Data/pondr_text_Nathan/", files[1]))
+
+## remove disorder segment lines:
+# 1. make logical vector for: if line contains "Predicted disorder segment"
 dis_seg <- grepl("Predicted disorder segment", pondr)
-all_stat <- pondr[!dis_seg]
+
+# 2. make new vector withough disorder segement lines
+all_stats <- pondr[!dis_seg]
+
 
 # isolate lines by each predictor
-vlxt <- all_stat[2:4]
-vl3 <- all_stat[6:8]
-vsl2 <- all_stat[10:12]
+vlxt <- all_stats[2:4]
+vl3 <- all_stats[6:8]
+vsl2 <- all_stats[10:12]
 
+# combine all predictors into single vect again (essentially removed head line)
 preds <- c(vlxt, vl3, vsl2)
+
+
+# create an empty vector for data
 myresult <- NULL
 
+# replace the first tab in every line with a ";" 
+# split line in substrings using the ; just inserted
+# extract number values form each substring and add to myresult vector
 for (p in preds) {
     sl <- sub("\t", ";", p)
-    z <- unlist(strsplit(sl, ";"))
+    z <- unlist(strsplit(sl, ";")) # srtsplit returns a list, but parse_number takes a vector
     m <- parse_number(z)
     myresult <- c(myresult, m)
 }
