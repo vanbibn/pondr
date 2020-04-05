1d53d673ef1c0302c75b606389eb0b472ff23597
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-04-04 21:12:51 -0400

    Initial commit for script to extract STRING protein interaction data from excel files. Plus two test files.

---
+++ Scripts/string_extract.R
@@ -0,0 +1,14 @@
+# Nathan W. Van Bibber
+# 2020-04-04
+# 
+# This is a script to extract data from sheets for an Excel file containing 
+# protein-protein interaction data from the STRING database.  Data were copied 
+# at medium (0.4), high (0.7), and highest (0.9) confidence for each protien, 
+# and pasted into Sheet1, Sheet2, Sheet3 respectuflly.
+
+
+# Load Packages
+library(readxl)
+library(tidyverse)
+
+
