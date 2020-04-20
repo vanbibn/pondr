5facde44984d8708610240d7b21ed16c29fb309a
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-04-19 23:23:05 -0400

    Testing different package names.

--- Scripts/aPackageName.R
+++ Scripts/aPackageName.R
@@ -7,3 +7,15 @@ library(available)
 available("pondr")
 
 available("IDmine")
+
+available("protDisorder")
+
+available("mineDisProt")  ## Winner!
+
+# Data Extraction of Intrinsically Disordered Proteins (DEIDP)
+available("DEIDP")
+available("deidp")
+available("dexIDP")
+
+# Data Extraction of Protein Intrinsic Disorder (DEPID)
+available("DEpID")
